d23ffb3ecf3008e603ee6c09f890a6bdf141f0d4
Alex McDonald <acm4219@gmail.com>
2020-08-10 18:54:25 -0500

    added temperatures to the cards

--- Assets/script.js
+++ Assets/script.js
@@ -21,26 +21,59 @@ function displayWeatherInfo(searchReq) {
     }).then(function(response) {
      console.log(response);
      console.log(response.list[0].main.temp);
+     //----Main Card
      var cityName = $('.cityname');
      var weatherIcon = $()   
      var cityTemp = $('.temperature');
      var cityHumid = $('.humidity');
      var cityWindSpeed = $('.windspeed');
-     var cityTemp1 = $('.temp1');
      var tempFar = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+     //----
+     //----Card One
+     var cityTemp1 = $('.temp1');
      var tempFarOne = (response.list[8].main.temp - 273.15) * 1.80 + 32;
+     //----
+     //----Card Two
+     var cityTemp2 = $('.temp2');
      var tempFarTwo = (response.list[16].main.temp - 273.15) * 1.80 + 32;
+     //----
+     //----Card Three
+     var cityTemp3 = $('.temp3');
      var tempFarThree = (response.list[24].main.temp - 273.15) * 1.80 + 32;
+     //----
+     //----Card Four
+     var cityTemp4 = $('.temp4');
      var tempFarFour = (response.list[32].main.temp - 273.15) * 1.80 + 32;
+     //----
+     //----Card Five
+     var cityTemp5 = $('.temp5');
      var tempFarFive = (response.list[39].main.temp - 273.15) * 1.80 + 32;
+     //----
      
      var tempFarInt = parseInt(tempFar);
      console.log(tempFarInt);
+     //----- Main Card
      cityName.html(response.city.name + ' ' + response.list[0].dt_txt);
      cityTemp.text('Temperature: ' + tempFar.toFixed(2));
      cityHumid.text('Humidity: ' + response.list[0].main.humidity);
      cityWindSpeed.text('Windspeed: ' + response.list[0].wind.speed);
-     cityTemp1.html('Temperature: ' + tempFarOne.toFixed(2));
+     //---------
+
+     //---------Card One
+     cityTemp1.text('Temperature: ' + tempFarOne.toFixed(2));
+     //---------
+     //----Card Two
+     cityTemp2.text('Temperature: ' + tempFarTwo.toFixed(2));
+     //----
+     //----Card Three
+     cityTemp3.text('Temperature: ' + tempFarThree.toFixed(2));
+     //----Card Four
+     cityTemp4.text('Temperature: ' + tempFarFour.toFixed(2));
+     //----
+     //----Card Five
+     cityTemp5.text('Temperature: ' + tempFarFive.toFixed(2));
+     //----
+
 
     });
 
